ce354ccccd5f362bd6db06c46fda880a5398d505
Mateus Moura <mateusvcsmoura@gmail.com>
2025-08-13 17:23:47 -0300

    Upload de Aula 08 | Enums

---
+++ src/08-enums.ts
@@ -0,0 +1,28 @@
+enum Direction {
+    North = 1,
+    South,
+    East,
+    West
+}
+
+const pos = {
+    x: 200,
+    y: 340,
+    direction: Direction.South // 2
+}
+
+//
+
+enum ClothingSize {
+    ExtraSmall = "PP",
+    Small = "P",
+    Medium = "M",
+    Large = "G",
+    ExtraLarge = "GG"
+}
+
+const cloth = {
+    color: "Green",
+    size: ClothingSize.Large // "G"
+}
+
